97563170f8426f5af7d4a4b3e60755020d211efd
cassiodasilva231-cmyk <cassiodasilva231@gmail.com>
2026-03-02 15:27:36 -0300

    Create temporada1.js

---
+++ temporada1.js
@@ -0,0 +1,26 @@
+const episodios = [
+{
+titulo:"EP1 - O Despertar",
+falas:[
+"Em um mundo dominado por energia espiritual...",
+"Kairo desperta seu poder.",
+"O eclipse começa..."
+]
+},
+{
+titulo:"EP2 - A Primeira Batalha",
+falas:[
+"Um inimigo surge.",
+"Kairo entra em combate.",
+"A batalha explode!"
+]
+},
+{
+titulo:"EP3 - Transformação",
+falas:[
+"Kairo atinge novo nível.",
+"A energia explode.",
+"O mundo treme."
+]
+}
+];
