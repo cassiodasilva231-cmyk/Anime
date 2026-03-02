d1376f77a9a7b42db288ca7902918cf0f34de7c4
cassiodasilva231-cmyk <cassiodasilva231@gmail.com>
2026-03-02 15:32:38 -0300

    Update temporada1.js

--- temporada1.js
+++ temporada1.js
@@ -1,26 +1,27 @@
 const episodios = [
+
 {
 titulo:"EP1 - O Despertar",
+tipo:"historia",
 falas:[
-"Em um mundo dominado por energia espiritual...",
-"Kairo desperta seu poder.",
-"O eclipse começa..."
+"Kairo era apenas um guerreiro comum...",
+"Até o dia em que o eclipse apareceu...",
+"Seu poder despertou."
 ]
 },
+
 {
-titulo:"EP2 - A Primeira Batalha",
-falas:[
-"Um inimigo surge.",
-"Kairo entra em combate.",
-"A batalha explode!"
-]
+titulo:"EP2 - O Inimigo",
+tipo:"luta",
+inimigo:"Soldado das Sombras",
+vida:120
 },
+
 {
-titulo:"EP3 - Transformação",
-falas:[
-"Kairo atinge novo nível.",
-"A energia explode.",
-"O mundo treme."
-]
+titulo:"EP3 - O Chefe",
+tipo:"boss",
+inimigo:"General Abissal",
+vida:220
 }
+
 ];
